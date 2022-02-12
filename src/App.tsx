4d56a39e14d9f6531fc80bda5458a47ba22c7827
Luan Bodner do Rosário <luan.rosario.bodner@gmail.com>
2022-02-11 21:16:34 -0300

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,79 @@
-import { AxiosResponse } from 'axios';
-import { WelcomeMessageDTO } from 'dtos/WelcomeMessageDTO';
-import React, { useEffect, useState } from 'react';
-import { NodeAPI } from 'services/Service';
-import { ListUsers } from 'views/ListUsers';
+import {
+  AppBar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState<string>('');
-
-  async function getWelcomeMessage() {
-    try {
-      const welcomeMessage: AxiosResponse<WelcomeMessageDTO> =
-        await NodeAPI.get(`${process.env.REACT_APP_API_URL}`);
-
-      console.log(welcomeMessage.data.mensagem);
-      setMessage(welcomeMessage.data.mensagem);
-    } catch (error) {
-      setMessage('Erro na chamada da API');
-    }
-  }
-
-  useEffect(() => {
-    getWelcomeMessage();
-  }, []);
+  const tabs = new Array<string>('Pagina inicial', 'Usuarios', 'Notas');
 
   return (
     <>
-      <ListUsers />
+      <Box sx={{ display: 'flex' }}>
+        <AppBar
+          sx={{
+            zIndex: 100,
+            backgroundColor: 'beige',
+          }}
+        >
+          <Toolbar>
+            <Typography variant={'h5'} color={'black'}>
+              Sistema de notas
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: 240,
+              zIndex: 1,
+              boxSizing: 'border-box',
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              {tabs.map((tab) => {
+                return (
+                  <ListItem
+                    component={Link}
+                    to={
+                      tab === 'Pagina inicial'
+                        ? '/'
+                        : tab === 'Usuarios'
+                        ? '/users'
+                        : '/notas'
+                    }
+                    button
+                  >
+                    <ListItemText primary={tab} />
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, paddingTop: '1.5%', paddingLeft: '15%' }}
+        >
+          <Toolbar />
+          <Routes>
+            <Route path="/" element={<>Texto qualquer</>} />
+          </Routes>
+        </Box>
+      </Box>
     </>
   );
 }
